89fd02027291a2a31f68391fc19857f616c71a8d
前行 <18826226743@163.com>
2025-04-23 10:20:43 +0800

    添加logo

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,17 +1,16 @@
 'use client'
 
 import Link from 'next/link'
+import Image from 'next/image'
 
 export function Header() {
   return (
     <header className="border-b bg-background">
       <div className="container flex h-16 items-center justify-between px-4">
         <div className="flex items-center gap-2">
-          <Link 
-            href="/" 
-            className="text-xl font-bold"
-          >
-            Text to Infographic
+          <Link href="/" className="flex items-center gap-2">
+            <Image src="/images/logo.png" alt="Logo" width={120} height={40} priority />
+            <span className="sr-only">Home</span>
           </Link>
         </div>
         
